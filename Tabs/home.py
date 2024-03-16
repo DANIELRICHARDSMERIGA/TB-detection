f69e587fff82c592d565e0c529891e73f5c48648
DANIELRICHARDSMERIGA <138503472+DANIELRICHARDSMERIGA@users.noreply.github.com>
2024-03-16 09:58:52 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -7,7 +7,7 @@ def app():
     """This function create the home page"""
     
     # Add title to the home page
-    st.title("Tuberculosis Type and Level Detector")
+    st.title("Enhance Tuberculosis Detection And Prediction")
 
     # Add image to the home page
     st.image("./images/home.png")
